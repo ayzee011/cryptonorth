be608ec83eca38dcb7fcfbaca4e11b5e910584e1
ayzee011 <voucher21@gmail.com>
2026-04-30 18:47:01 -0700

    Add Google Analytics G-X6LMX9VR0Y to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-      </head>
+      
+      {/* Google Analytics */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-X6LMX9VR0Y"></script>
+      <script dangerouslySetInnerHTML={{__html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-X6LMX9VR0Y');
+      `}} />
+    </head>
       <body>{children}</body>
     </html>
   )

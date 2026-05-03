3e7044a8bb3b801cbdcbed94390cbc79244052c9
ayzee011 <voucher21@gmail.com>
2026-05-03 16:26:12 -0700

    Fix GA Measurement ID to G-TEW3DZK54F

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,12 +15,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
       
       {/* Google Analytics */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-X6LMX9VR0Y"></script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-TEW3DZK54F"></script>
       <script dangerouslySetInnerHTML={{__html: `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-X6LMX9VR0Y');
+        gtag('config', 'G-TEW3DZK54F');
       `}} />
     </head>
       <body>{children}</body>

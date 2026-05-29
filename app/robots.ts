d90604a16e9b3bd8c83d204be6fc8af71c6f9e2f
ayzee011 <voucher21@gmail.com>
2026-05-29 03:44:07 -0700

    Fix robots.txt canonical to cryptonorth.ca

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,7 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: 'https://www.cryptonorth.ca/sitemap.xml',
+    rules: { userAgent: '*', allow: '/' },
+    sitemap: 'https://cryptonorth.ca/sitemap.xml',
   }
 }

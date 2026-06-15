71e686ec60fb840253380e9e348add997268c698
ayzee011 <voucher21@gmail.com>
2026-06-15 07:58:55 -0700

    Fix canonical URLs to www.cryptonorth.ca

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,6 @@ import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://cryptonorth.ca/sitemap.xml',
+    sitemap: 'https://www.cryptonorth.ca/sitemap.xml',
   }
 }
